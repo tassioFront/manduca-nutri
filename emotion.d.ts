e3d8ab805ba837c4357101f431b1c482136c2c7c
Tássio <tassio72@hotmail.com>
2023-03-13 19:25:05 -0300

    chore: add material

---
+++ emotion.d.ts
@@ -0,0 +1,10 @@
+declare module "@emotion/react" {
+  export interface Theme {
+    palette: {
+      bgPink: {
+        main: string;
+        dark: string;
+      };
+    };
+  }
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+const colors = {
+  palette: {
+    primary: {
+      main: "#00A884",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: red.A400,
+    },
+    bgPink: {
+      main: "#DD978D",
+      light: "#FFEEEB",
+      dark: "#70241a",
+    },
+  },
+};
+const theme = createTheme({ ...colors });
+export { colors };
+export default theme;
